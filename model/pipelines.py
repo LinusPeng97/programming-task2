bf6bae7b80c4d853ed406f99de91f72cc5035375
LinusPeng97 <40586980+LinusPeng97@users.noreply.github.com>
2019-12-12 17:03:40 +0800

    add issueInfo

--- model/pipelines.py
+++ model/pipelines.py
@@ -4,9 +4,9 @@ import csv
 class ModelPipeline(object):
 
     def process_item(self, item, spider):
-        f = open('issusInfo.csv', 'a+', encoding='utf-8-sig')
+        f = open('issueInfo.csv', 'a+', encoding='utf-8-sig')
         writer = csv.writer(f)
-        writer.writerow((item['Type'], item['Assignee'], item['Created'], item['Created_Epoch'], item['Description'], item['Comments']))
+        writer.writerow((item['Issue'], item['Type'], item['Assignee'], item['Created'], item['Created_Epoch'], item['Description'], item['Comments']))
         return item
 
 
